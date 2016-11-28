8fa47104a92c03cbb6cdbe2cbad1b6cbeb3384ef
adraicbrom <asabrom@hotmail.com>
2016-11-28 17:25:00 -0600

    added comments

--- take_save.py
+++ take_save.py
@@ -1,13 +1,16 @@
+#Script continually takes images from the camera and saves them in a numbered sequence
+
 import SimpleCV
 
-directory = "/home/asa/Documents/StudentLaunch/"
-img_name = "test_image_"
+directory = "/home/asa/Documents/StudentLaunch/" #define save directory
+img_name = "test_image_" #define image file name
+img_type = ".png" #define image file type
 
 cam = SimpleCV.Camera()
 i = 0
 while True:
 	img = cam.getImage()
-	img.save(directory+img_name+str(i)+".png")
+	img.save(directory+img_name+str(i)+img_type)
 	i = i+1
 	break #take out to continually take and save images
 
