8046c87a180794073847ef8491d58fa7ff5e17ed
Asa <asabrom@gmail.com>
2016-12-09 20:39:56 -0600

    Added resolution change

--- take_save.py
+++ take_save.py
@@ -1,16 +1,18 @@
 #Script continually takes images from the camera and saves them in a numbered sequence
 
-import SimpleCV
+from SimpleCV import *
+import time
 
 directory = "/home/asa/Documents/StudentLaunch/" #define save directory
 img_name = "test_image_" #define image file name
 img_type = ".png" #define image file type
 
 cam = SimpleCV.Camera()
+#cam = Camera(prop_set={'width':1920, 'height':1080}) #replace previous line with this to set resolution
 i = 0
-while True:
+while i<4000:
 	img = cam.getImage()
 	img.save(directory+img_name+str(i)+img_type)
 	i = i+1
-	break #take out to continually take and save images
+	#break #comment out to continually take and save images
 #testing testing
