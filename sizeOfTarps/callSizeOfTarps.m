1f2798e0ebe880c0c5b0d9de3b776ac9327ff754
Gutofoo <guto@de-oliveira.org>
2016-12-09 21:03:49 -0600

    Overlay +300 & -300
    Overlay both  the +300 and -300 possibilities on the same plot. It’s a
    mess right now.

--- sizeOfTarps/callSizeOfTarps.m
+++ sizeOfTarps/callSizeOfTarps.m
@@ -18,12 +18,13 @@ for i = 1:length(altitude)
     end
 end
 
-range = linspace(-2500+300,2500+300,2001);
+range1 = linspace(-2500+300,2500+300,2001);
+range2 = linspace(-2500-300,2500-300,2001);
 %+300 and -300 on the min and max of this manipulates location of tarps in
 %relation to the launch pad
-
+%two ranges to overlay different both +300 and -300 at the same time
 for i = 1:length(altitude)
-    for j = 1:length(range)
+    for j = 1:length(range1)
         if (j<1001)
             numPixels2(i,j) = numPixels(i,1002-j);
         else
@@ -35,8 +36,9 @@ for i = 1:length(altitude)
     end
 end
 v = [300, 500, 800, 1200, 1700, 2300, 3000, 4000, 6000, 20000];
-[C,h] = contour(range,altitude,numPixels2,v); %someone please epxlain wtf this is?
+[C,h] = contour(range1,altitude,numPixels2,v); %someone please epxlain wtf this is?
 hold on;
+[C,h] = contour(range2,altitude,numPixels2,v);
 plot(data10(:,2),data10(:,1),'Linewidth',2,'Color','b')
 plot(data20(:,2),data20(:,1),'Linewidth',2,'Color','r')
 clabel(C,h,v)
