745438441e7ad79fb55e16d715427305b4b4a4e2
adraicbrom <asabrom@gmail.com>
2017-02-15 18:48:42 -0600

    edited comments

--- code/test.py
+++ code/test.py
@@ -7,7 +7,6 @@ test_rect = 0
 test_tolerances = 0
 test_pic_time_1080 = 1
 test_pic_time_max = 1
-#print time.now()
 
 # Show rectangles in pic
 if(test_rect):
@@ -20,20 +19,21 @@ if(test_rect):
 	img.show()
 	time.sleep(3)
 	
+#Saves 100 versions of test file showing various tolerances of blobs rect
 if(test_tolerances):
-	#img = Image("test_images/test11.png")
 	for i in range(0,100):
 		img = Image("test_images/test11.png")
 		rects = find_rects(img, i/100)
 		for rect in rects:
 			rect.drawOutline((128,0,0),-1,4)
 		
+#Logs time to take and save a 1920x1080 pic
 if(test_pic_time_1080):
 	width = 1920
 	height = 1080
 	cam = SimpleCV.Camera(prop_set={'width':width, 'height':height})
 	i = 0
-	write_log("1920x1080")
+	write_log("\n1920x1080\n")
 	while(i<100):
 		t1 = datetime.now()
 		img = capture_image(cam)
@@ -44,12 +44,13 @@ if(test_pic_time_1080):
 		print t
 		write_log(t)
 
+#Logs time to take and save a 3280x2464 pic
 if(test_pic_time_max):
 	width = 3280
 	height = 2464
 	cam = SimpleCV.Camera(prop_set={'width':width, 'height':height})
 	i = 0
-	write_log("1920x1080")
+	write_log("\n3280x2464\n")
 	while(i<100):
 		t1 = datetime.now()
 		img = capture_image(cam)
