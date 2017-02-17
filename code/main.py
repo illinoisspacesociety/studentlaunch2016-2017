9ee8b913c7189648e6ffc9a3401c62a305081ef9
adraicbrom <asabrom@gmail.com>
2017-02-16 19:02:47 -0600

    added more tests

--- code/main.py
+++ code/main.py
@@ -9,6 +9,7 @@ import time
 WIDTH = 0
 HEIGHT = 0
 RECT_TOLERANCE = 0.5
+COLOR_TOLERANCE = 100
 DIRECTORY = "/home/asa/Documents/StudentLaunch/studentlaunch2016-2017/images/"
 IMAGE_NAME = "image_"
 IMAGE_TYPE = ".png"
@@ -38,21 +39,37 @@ def get_size():
 	
 # Function to find rectanges in given image
 def find_rects(img, tol=RECT_TOLERANCE):
+	print tol
 	blobs = img.findBlobs()
 	if not blobs:
 		return 0
 	rects = blobs.filter([b.isRectangle(tol) for b in blobs])
 	return rects
 	
+# Function to determin if the image has successfully seen the tarps
 def check_success():
 	##TODO: write function
 	return 1
 	
+# Function to check that blob matches target color
+def check_color(blob,target):
+	color = blob.meanColor()
+	match = (0,0,0)
+	for i in range(1,3):
+		if(color[i] >= target[i] - COLOR_TOLERANCE and color[i] <= target[i] + COLOR_TOLERANCE):
+			match[i] = 1
+		i += 1
+	if(color[1] and color[2] and color[3]):
+		return 1
+	return 0
+	
+# Function to save the image in the given directory
 def save_image(img):
 	t = datetime.now().strftime("%Y%m%d_%H%M%S%f")
 	img.save(DIRECTORY+IMAGE_NAME+t+IMAGE_TYPE)
 	return 1
 
+# Function to write given info to log file
 def write_log(log):
 	f = open('log','a+')
 	f.seek(0,2)
--- code/test.py
+++ code/test.py
@@ -1,19 +1,21 @@
 from main import *
 from SimpleCV import *
-#import time
-from datetime import *
+import time
+from datetime import datetime, timedelta
 
 test_rect = 0
 test_tolerances = 0
-test_pic_time_1080 = 1
-test_pic_time_max = 1
+test_pic_time_1080 = 0
+test_pic_time_max = 0
+test_blob_colors = 1
+test_color_matching = 0
 
 # Show rectangles in pic
 if(test_rect):
 	#cam = SimpleCV.Camera()
 	#img = capture_image(cam)
 	img = Image("test_images/test11.png")
-	rects = find_rects(img)
+	rects = find_rects(img, 0.4)
 	for rect in rects:
 		rect.drawOutline((128,0,0),-1,4)
 	img.show()
@@ -21,11 +23,14 @@ if(test_rect):
 	
 #Saves 100 versions of test file showing various tolerances of blobs rect
 if(test_tolerances):
-	for i in range(0,100):
+	i = 0
+	while(i<100):
 		img = Image("test_images/test11.png")
-		rects = find_rects(img, i/100)
+		rects = find_rects(img, float(i)/100)
 		for rect in rects:
 			rect.drawOutline((128,0,0),-1,4)
+		save_image(img)
+		i += 1
 		
 #Logs time to take and save a 1920x1080 pic
 if(test_pic_time_1080):
@@ -43,7 +48,8 @@ if(test_pic_time_1080):
 		t = str(dt.seconds*1000000 + dt.microseconds)
 		print t
 		write_log(t)
-
+		i += 1
+		
 #Logs time to take and save a 3280x2464 pic
 if(test_pic_time_max):
 	width = 3280
@@ -58,5 +64,18 @@ if(test_pic_time_max):
 		t2 = datetime.now()
 		dt = t2 - t1
 		t = str(dt.seconds*1000000 + dt.microseconds)
+		print i
 		print t
 		write_log(t)
+		i += 1
+
+#Prints the colors of blobs in a pic
+if(test_blob_colors):
+	img = Image("test_images/test11.png")
+	rects = find_rects(img)
+	for rect in rects:
+		print rect.meanColor()
+
+#Tests if blobs color near target		
+if(test_color_matching):
+	print "testing"
